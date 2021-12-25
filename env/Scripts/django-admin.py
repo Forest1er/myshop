c001464bbe212dc6f94be59e7b9689f0fe9cef54
Ildar <Ildar@TUF>
2021-12-25 14:53:57 +0300

    123

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ildar\Desktop\3 Курс\1 семестр\Ввит\myshop\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- env1/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Ildar\Desktop\3 Курс\1 семестр\Ввит\myshop\env1\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
